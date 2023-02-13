db7861882497000f58ad71e60fa4f7cb07f225dc
bogdanvuksanovic <bogdan.vuksanovic@dev.telnet.rs>
2023-02-13 14:36:13 +0100

    some corrections

--- shop-cart/script.js
+++ shop-cart/script.js
@@ -1,43 +1,58 @@
 let totalItems = 0;
 function addToCart(element){
-let container = element.closest(`.item`);
-let price = container.querySelector(`.price`).innerText;
-let name = container.querySelector(`h3`).innerText;
-let quantity = container.querySelector(`.quantity`).value;
-let items = document.querySelector(`.items`);
-let total = document.querySelector(`.total`);
+    let container = element.closest(`.item`);
+    let price = container.querySelector(`.price`).innerText;
+    let name = container.querySelector(`h3`).innerText;
+    let quantity = container.querySelector(`.quantity`).value;
+    let items = document.querySelector(`.items`);
+    let total = document.querySelector(`.total`);
 
-//console.log(total);
+    //console.log(total);
 
-//console.log(typeof(quantity))
-let quantityInt = parseInt(quantity);
+    //console.log(typeof(quantity))
+    let quantityInt = parseInt(quantity);
 
-//console.log(typeof(quantityInt))
+    //console.log(typeof(quantityInt))
 
 
 
-if(quantityInt > 0){
+    if(quantityInt > 0){
 
-    let totalItem = quantityInt * price;
-    totalItems += totalItem;
-    items.innerHTML += `
-    <div class="selected-items">
-        <h3>${name}</h3>
-        <p>Cena: ${price}<br>Kolicina: ${quantityInt}<br>Cena: ${price}
-        <button id="dltBtn" onClick="removeItem()">Remove Item</button>
-    </div>
-    `;
+        let totalItem = quantityInt * price;
+        totalItems += totalItem;
+        items.innerHTML += `
+        <div class="selected-items">
+            <h3>${name}</h3>
+            <p>Cena: ${price}<br>Kolicina: ${quantityInt}<br>Cena: <span>${totalItem}</span>
+            <button id="dltBtn" onClick="removeItem(this)">Remove Item</button>
+        </div>
+        `;
 
-    element.innerText = `Added`;
-    element.setAttribute(`disabled`,``);
+        element.innerText = `Added`;
+        element.setAttribute(`disabled`,``);
 
-    total.innerHTML = `<hr>Ukupan racun: ${totalItems}`
+        total.innerHTML = `<hr>Ukupan racun: ${totalItems}`;
 
-    console.log(totalItems)
+        console.log(totalItems)
 
-}else{
-    console.log(`Dodati kolicinu`)
+    }else{
+        console.log(`Dodati kolicinu`)
+    }
 }
 
+function removeItem(element){
+
+    let container = element.closest(`.selected-items`);
+    let totalItem = container.querySelector(`p span`).innerText;
+    let total = document.querySelector(`.total`);
+    //console.log(price)
+    totalItem = parseInt(totalItem);
+
+    console.log(typeof(totalItems))
+
+    totalItems -= totalItem;
+    total.innerHTML = `<hr>Ukupan racun: ${totalItems}`;
+
+    container.remove();
 
 }
