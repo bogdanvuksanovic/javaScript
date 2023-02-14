c0a1991b62f7b60ab50fa4cc46fb3b8f9718cab2
bogdanvuksanovic <bogdan.vuksanovic@dev.telnet.rs>
2023-02-14 12:02:21 +0100

    added css and some correction for html

--- shop-cart/script.js
+++ shop-cart/script.js
@@ -1,6 +1,7 @@
 let totalItems = 0;
 function addToCart(element){
-    let container = element.closest(`.item`);
+    let productsInCart = [];
+    let container = element.closest(`.one-item`);
     let price = container.querySelector(`.price`).innerText;
     let name = container.querySelector(`h3`).innerText;
     let quantity = container.querySelector(`.quantity`).value;
@@ -25,12 +26,13 @@ function addToCart(element){
         <div class="selected-items">
             <h3>${name}</h3>
             <p>Cena: ${price}<br>Kolicina: ${quantityInt}<br>Cena: <span>${totalItem}</span>
-            <button id="dltBtn" onClick="removeItem(this)">Remove Item</button>
+            <button id="dltBtn" onClick="removeItem(this)">Obrisati porudzbinu</button>
         </div>
         `;
 
+
         element.innerText = `Dodato`;
-        element.setAttribute(`disabled`,``);
+        //element.setAttribute(`disabled`,``);
 
         total.innerHTML = `<hr>Ukupan racun: ${totalItems}`;
 
