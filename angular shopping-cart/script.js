499dd444d639502538518847173bc72a14fbc503
bogdanvuksanovic <bogdan.vuksanovic@dev.telnet.rs>
2023-03-14 09:26:27 +0100

    some updates

--- angular shopping-cart/script.js
+++ angular shopping-cart/script.js
@@ -1,34 +1,20 @@
-var app = angular.module("shoppingCart", [])
-                 .controller("cartCtrl", function($scope, $http) {
-  $http.get("http://localhost:3000/items")
-      .then(function(response){
-        $scope.items = response.data;
-      });
-      $scope.cartTotal = function() {
-        var totalPrice = 0;
-        angular.forEach($scope.items, function(item) {
-            totalPrice += item.price * item.quantity;
-        });
-        return totalPrice;
-    };
-    
-    $scope.updateCart = function() {
-      // Recalculate cart total
-    };
-      
-  })
+angular.module("shoppingCart", [])
+        .controller("cartCtrl", function($http) {
+            var vm = this;
+            $http.get("http://localhost:3000/items")
+                .then(function(response){
+                  vm.items = response.data;
+                });
+              vm.cartTotal = function() {
+                  var totalPrice = 0;
+                  angular.forEach(vm.items, function(item) {
+                      totalPrice += item.price * item.quantity;
+                  });
+                  return totalPrice;
+              };
 
-  // $scope.updateCart = function() {
-  //   // Recalculate cart total
-  // };
-  // $scope.cartTotal = function() {
-  //   var total = 0;
-  //   // angular.forEach($scope.items , function(total) {
-  //   //   total += $scope.items[0].quantity * $scope.items[0].price;
-  //   // });
-  //   for (var i = 0; i < items.lenght; i++) {
-  //     total += $scope.items[i].quantity * $scope.items[i].price;
-  //   }
-  //   return total;
-  // };
+              vm.updateCart = function() {
 
+              };
+
+            })
