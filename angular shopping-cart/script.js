14c66adc552271a4e97dbef6a77ea80cab162155
bogdanvuksanovic <bogdan.vuksanovic@dev.telnet.rs>
2023-03-15 15:46:07 +0100

    added functions

--- angular shopping-cart/script.js
+++ angular shopping-cart/script.js
@@ -25,25 +25,54 @@ angular
                  .then(function(response){
                     vm.items = response.data;
                  })
-                 vm.cartItems = [];
 
-                 vm.addToCart = function(product) {
-                     // Check if product is already in cart
-                     var cartItemIndex = vm.cartItems.findIndex(function(item) {
-                         return item.product.name === product.name;
-                     });
+                vm.cartItems = [];
+                vm.addToCart = function(product) {
+                    if (!Array.isArray(vm.cartItems)) {
+                        vm.cartItems = [];
+                    }
+                    // Check if product is already in cart
+                    var cartItemIndex = vm.cartItems.findIndex(function(item) {
+                        return item.product.name === product.name;
+                    });
              
-                     if (cartItemIndex === -1) {
-                         // Add new cart item
-                         vm.cartItems.push({
-                             product: product,
-                             quantity: 1
-                         });
-                     } else {
-                         // Increase quantity of existing cart item
-                         vm.cartItems[cartItemIndex].quantity++;
-                     }
-                 };
+                    if (cartItemIndex === -1) {
+                        // Add new cart item
+                        vm.cartItems.push({
+                            product: product
+                        });
+                    } else {
+                        // Increase quantity of existing cart item
+                        vm.cartItems[cartItemIndex].product.quantity++;
+                    }
+
+                    console.log(vm.cartItems)
+                };
+
+
+                vm.cartTotal = function() {
+                    var totalPrice = 0;
+                    angular.forEach(vm.items, function(item) {
+                        totalPrice += item.price * item.quantity;
+                    });
+                    console.log(totalPrice)
+                    return totalPrice;
+                };
+                console.log(vm.cartTotal())
+
+                vm.clearCart = function(){
+                    vm.cartItems = 0;
+                    //vm.cartTotal();
+                }
+
+                vm.removeItem = function(item) {
+                    var index = vm.cart.indexOf(item);
+                    vm.cart.splice(index, 1);
+                  };
+    
+                vm.updateCart = function() {
+                    vm.cartTotal()
+                };
         }
 
         // .controller("itemsController", function($http, $state) {
@@ -72,13 +101,13 @@ angular
                 .then(function(response){
                   vm.items = response.data;
                 });
-            vm.cartTotal = function() {
-                var totalPrice = 0;
-                angular.forEach(vm.items, function(item) {
-                    totalPrice += item.price * item.quantity;
-                });
-                return totalPrice;
-            };
+            // vm.cartTotal = function() {
+            //     var totalPrice = 0;
+            //     angular.forEach(vm.items, function(item) {
+            //         totalPrice += item.price * item.quantity;
+            //     });
+            //     return totalPrice;
+            // };
 
             vm.updateCart = function() {
                 vm.cartTotal()
