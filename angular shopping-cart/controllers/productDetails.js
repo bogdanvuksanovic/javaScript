31320b27f53ddd65706d4a01ec83b2ee9ba494f1
bogdanvuksanovic <bogdan.vuksanovic@dev.telnet.rs>
2023-04-12 12:00:49 +0200

    added getProductDetails in cart service and call it in controller

--- angular shopping-cart/controllers/productDetails.js
+++ angular shopping-cart/controllers/productDetails.js
@@ -6,17 +6,12 @@ function productDetailsController($http, $stateParams, cartService) {
 	var vm = this;
 	vm.cart = [];
 
-	$http({
-		url: 'http://localhost:3000/items',
-		params: { id: $stateParams.id },
-		method: 'get'
-	}).then(function (response) {
-		vm.item = response.data[0];
-		console.log(response);
+	cartService.getProductDetails().then(function (response) {
+		vm.item = response;
 	});
 
 	vm.addToCart = function (product) {
-		cartService.addToCart(product);
+		cartService.addToCart(product, vm.cart);
 		cartService.getCartData().then(function (response) {
 			vm.cart = response;
 		});
